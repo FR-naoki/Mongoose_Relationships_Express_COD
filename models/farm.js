241e8de30f65720db53e8fcd880a6a626b11b162
FR <naoki.maekawa.511@gmail.com>
2023-11-17 20:27:50 +0900

    11/17

--- models/farm.js
+++ models/farm.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const Product = require('./Product')
+
 const farmSchema = new Schema({
     name:{
         type: String,
@@ -26,9 +28,11 @@ const farmSchema = new Schema({
 });
 
 
-farmSchema.post(`findOneAndDelete`, async function (data) {
-    console.log(`POST middleware!!!`);
-    console.log(data);
+farmSchema.post(`findOneAndDelete`, async function (farm) {
+    if (farm.products.length) {
+       const res = await Product.deleteMany({_id:{$in:farm.products}});
+       console.log(res);
+    }
 })
 
 const Farm = mongoose.model(`Farm`, farmSchema);
